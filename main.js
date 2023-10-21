baccc3d4005370a2e3b5956b45fcfac164ef1c9c
AnwiSat1288 <147772201+AnwiSat1288@users.noreply.github.com>
2023-10-21 13:56:14 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+var firebaseConfig = {
   apiKey: "AIzaSyCFU1OjM0k92TwxMoZFJzVTsBt3ay87oyM",
   authDomain: "one-world-github-pages.firebaseapp.com",
   projectId: "one-world-github-pages",
